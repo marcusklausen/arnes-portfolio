30876ac0c3210c324d59850fa309d33e05ef6b72
Marcus Klausen <marcusklausen@Marcus-MBP.local>
2021-01-29 08:48:35 +0100

    cleaned up kontakt.php for readability, added foto library and foto-album.php page

--- foto-album.php
+++ foto-album.php
@@ -45,54 +45,40 @@
     <div class="wrapper box-shadow flex-parent">
         <main class="main-content">
 
-            <h1>VELKOMMEN TIL ARNES' PORTFOLIO</h1>
-            <p>
-                Hej, jeg hedder Arne Nougatgren og jeg er musikalsk socialpædagog og rytmeterapeut, med en særlig
-                forkærlighed for psykedelisk jazz-funk, -i daglig tale: psyzzunk. Jeg arbejder hårdt på at skabe
-                bevidsthed om de pædagogiske muligheder der kan skabes i det psyzzunkiske rum.
-            </p>
-            <p>
-                Det burde være indlysende for enhver, men fakta er at kendskabet til psyzzunkens pædagoiske perspektiver
-                er enormt begrænset, i dagens Danmark, om man så må sige.
-            </p>
-            <img src="img/arne.jpg">
+            <h1>Foto album</h1>
+            <div class="gallery">
+                <a href="img/content/galleri1.jpg" target="expand-image">
+                    <img src="img/content/galleri1.jpg">
+                </a>
+                <a href="img/content/galleri2.jpg" target="expand-image">
+                    <img src="img/content/galleri2.jpg">
+                </a>
+                <a href="img/content/galleri3.jpg" target="expand-image">
+                    <img src="img/content/galleri3.jpg">
+                </a>
+                <a href="img/content/galleri4.jpg" target="expand-image">
+                    <img src="img/content/galleri4.jpg">
+                </a>
+                <a href="img/content/galleri5.jpg" target="expand-image">
+                    <img src="img/content/galleri5.jpg">    
+                </a>
+                <a href="img/content/galleri6.jpg" target="expand-image">
+                    <img src="img/content/galleri6.jpg">
+                </a>
+                <a href="img/content/galleri7.jpg" target="expand-image">
+                    <img src="img/content/galleri7.jpg">
+                </a>
+                <a href="img/content/galleri8.jpg" target="expand-image">
+                    <img src="img/content/galleri8.jpg">
+                </a>
+                <a href="img/content/galleri9.jpg" target="expand-image">
+                    <img src="img/content/galleri9.jpg">
+                </a>
+            </div>
         </main>
 
         <aside class="sidebar">
-            <h2>CSS FLEXBOX</h2>
-            <p>
-                Denne svedige side er bygget op med flexbox modellen. Så slipper man for at bruge floats/ clear,
-                inline-block, display:table eller andre nederen hacks.
-            </p>
-
-            <h2>BROWSER SUPPORT:</h2>
-            <p>Flexbox modellen er efterhånden rigtig godt understøttet i alle moderne browsere.</p>
-
-            <h2>HVORDAN GØR MAN, ARNE?</h2>
-            <p>
-                Jo... For at få sine elementer til at tilpasse sig flexbox modellen, skal man altså lige gide give deres
-                umidelbare forældre-element display-proprtien flex
-            </p>
-
-            <code>
-            .flex-container { display:flex; }
-            </code>
-            <p>
-                Derefter vil alle direkte børn af .flex-conationer placere sig i en række. Det er pædagogik der vil noget.
-
-            </p>    
-            <h2>FORDEL JER LIDT, UNGER</h2>
-            <p>
-                Hvis man li'som vil have alle flex-børnene til at fordele sig over hele bredten, så kan man lige tilføje en
-                justify-content property med værdien space-between
-
-            </p>    
-            <code>
-                .flex-container {
-                    display:flex;
-                    justify-content: space-between;
-                }
-            </code>
+            <iframe src="img/content/galleri9.jpg" frameborder="0" name="expand-image" class="expand-image"></iframe>
         </aside>
 
     </div>
@@ -108,7 +94,7 @@
                     1313 Andeby
                 </p>
             </section>
-            <section>
+            <section> 
                 <h3>Get in touch</h3>
                 <a href="#">Kontakt</a>
                 <a href="#">Facebook</a>
